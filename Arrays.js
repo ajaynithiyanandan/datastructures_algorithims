ecb56b8259c799970fb26b5fc07152a30208fa76
Ajay Nithiyanandan <ajay.nithiyanandan89@gmail.com>
2024-12-28 07:21:11 -0700

    Sample code

--- Arrays.js
+++ Arrays.js
@@ -1,14 +1,19 @@
 const nemo = ['nemo'];
 
-function findNemo(array){
+const characters = ['Marlin', 'Dory', 'Gill', 'Bloat', 'Peach', 'Gurgle', 'Deb', 'Bubbles', 'Nigel', 'Crush', 'Squirt', 'Bruce', 'Anchor', 'Chum', 'Nemo'];
+
+const large = new Array(10000).fill('nemo');
+
+function findNemo(arrayname,array){
     let t0 = performance.now();
     for (let i=0;i<array.length;i++){
         if(array[i] === 'nemo'){
-            console.log('Found Nemo');
+            // console.log('Found Nemo');
         }
     }
     let t1 = performance.now();
-    console.log('Call to find Nemo took ' + (t1-t0) + ' milliseconds');
+    console.log('Call to find Nemo using array ' + arrayname + 'took ' + (t1-t0) + ' milliseconds');
 }
-
-findNemo(nemo);
+findNemo('nemo',nemo);
+findNemo('characters',characters);
+findNemo('large',large);
